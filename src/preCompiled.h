a84b5afa3c08db21bc5902344a79d3126c1f0a78
Ellie <ellie1337@gmail.com>
2014-03-26 16:29:34 -0700

    Move these to their own file, and expand it based on Microsoft's implementation.

--- src/preCompiled.h
+++ src/preCompiled.h
@@ -4,25 +4,16 @@
 #include <Windows.h>
 #include <commctrl.h>
 #include <commdlg.h>
-#include <tchar.h>
 
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <map>
 #include <vector>
+
+#include "tcharness.h"
 using namespace std;
 
 #include "resource.h"
-
-typedef std::basic_string<TCHAR, char_traits<TCHAR>, allocator<TCHAR> > tstring;
-typedef std::basic_stringstream<TCHAR,  char_traits<TCHAR>, allocator<TCHAR> > tstringstream;
-#ifdef UNICODE
-#define to_tstring to_wstring
-#else
-#define to_tstring to_string
-#endif
-
 #include "DBCFile.h"
-
 #include "Velvet.h"
